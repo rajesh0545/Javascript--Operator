0a5c05300375cf4d18e97bbf9274fd04a32dd19c
rajesh <krajeshgowri7032@mail.com>
2022-08-16 16:55:05 +0530

    javascript-comparsion

---
+++ script.js
@@ -0,0 +1,12 @@
+//is equal to
+console.log(2==2);
+console.log(2=='2');
+//not equal to 
+console.log(7!==2);
+console.log('hello'==hello);
+//Identical is equal to
+console.log(2===2);
+console.log(2==='2');
+//Not identical
+console.log(!8===2);
+console.log(!8==='2');
